63ddb48ac2c50a443b1be760ea6ba5e687b2b8f9
corozo234 <elpepaderleycorozo@outlook.com>
2022-07-18 18:18:07 -0500

    Examen practico

---
+++ practica.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+
+double Factorial(int num)
+{
+    double fac = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fac *= i;
+    }
+    return fac;
+}
+int Potencia (int base, int exp)
+{
+    int res = 1;
+    for (int j = 1; j <= exp; j++)
+    {
+        res *= base;
+    }
+    return res;
+}
+double Serie(int num)
+{
+    double s=0;
+    for (int k = 2; k <= num; k++)
+    {
+        s += (Factorial (k) / Potencia (6, k-1));
+    }
+    return s;
+}
+
+int main()
+{
+    Serie(4);
+    cout<<endl<<"El factorial de 4 es "<<Factorial(4);
+
+    getch();
+    return 0;
+}
